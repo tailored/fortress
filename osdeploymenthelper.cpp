3735762803ca62118cbb58ffe2a1a210b05a5238
Christian Richter <christian@boltares.de>
2014-09-02 14:07:04 +0200

    - fixed debian/ubuntu deployment

--- osdeploymenthelper.cpp
+++ osdeploymenthelper.cpp
@@ -12,8 +12,8 @@ void OsDeploymentHelper::ubuntuDeploy() {
                     QStringList() << QString("cp /etc/rc.local /etc/rc.local.fortressbackup"));
     QFile file("/etc/rc.local");
     QString rcLocalContents = file.readAll();
-    if(!rcLocalContents.contains("/echo 0/")) {
-        rcLocalContents.replace("/echo 0/",QString(FORTRESS_RULES_BOOT_DEPLOYMENT_PATH).append("firewall.sh\nexit 0"));
+    if(rcLocalContents.contains("/exit 0/") && rcLocalContents.contains(QString("/").append(FORTRESS_RULES_BOOT_DEPLOYMENT_PATH).append("firewall.sh").append("/"))) {
+        rcLocalContents.replace("/exit 0/",QString(FORTRESS_RULES_BOOT_DEPLOYMENT_PATH).append("firewall.sh\nexit 0"));
         process.execute(SettingsManager::getSharedInstance()->getValue("settings/sudoprovider"),
                         QStringList() << QString("echo `").append(rcLocalContents).append("`"));
     }
@@ -23,11 +23,6 @@ void OsDeploymentHelper::ubuntuDeploy() {
 void OsDeploymentHelper::fedoraDeploy() {
 
 }
-
-void OsDeploymentHelper::debianDeploy() {
-
-}
-
 
 
 void OsDeploymentHelper::gentooRemove() {
@@ -48,9 +43,5 @@ void OsDeploymentHelper::ubuntuRemove() {
 }
 
 void OsDeploymentHelper::fedoraRemove() {
-
-}
-
-void OsDeploymentHelper::debianRemove() {
 
 }
